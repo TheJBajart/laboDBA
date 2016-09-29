12dc8cea8223206559f5fe9590bd0125b9cb4f24
TheJBajart <jerome-bajart@outlook.be>
2016-09-29 16:04:31 +0200

    test

---
+++ Labo1BDA/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Labo1BDA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Tets;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            String sql = "select s.fullname, s.Birthdate, s.Remark,c.Description from dbo.student s,dbo.Course c, dbo.StudentCourse sC where sC.StudentId = s.Id AND sC.CourseId = c.Id;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sql, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    String saveName = "";
+                    while (reader.Read())
+                    {
+                        if(saveName.Equals("") || !saveName.Equals(reader[0]))
+                        {
+                            Console.WriteLine("nom :\t{0} \n date anniversaire : \t{1} \n note : \t{2} \n cours : \t{3}", reader[0], reader[1],reader[2],reader[3]);
+                        }
+                        Console.WriteLine(",\t{3}",reader[3]);
+                        saveName = (string)reader[0];
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+
+            }
+            
+        }
+    }
+}
---
+++ labo2ETFramework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labo2ETFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 0;
+            TetsEntities contexte = new TetsEntities();
+            foreach (Student s in contexte.Student)
+            {
+                foreach(Course c in contexte.Course)
+                {
+                    if (id != s.Id)
+                    {
+                        Console.Write(s.FullName + "\n");
+                    }
+                    Console.Write(c.Description +"\n");
+                    id = (int)s.Id;
+                }
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
